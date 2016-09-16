9d67b800bead410623ca61bded1b30bb02cacbbb
WodeCraft <jcr@wodecraft.dk>
2016-09-16 11:19:01 +0200

    Exercise 2 - Table layout

--- MbmStore/Models/Movie.cs
+++ MbmStore/Models/Movie.cs
@@ -1,54 +1,13 @@
-﻿using System;
-
-namespace MbmStore.Models
+﻿namespace MbmStore.Models
 {
-    public class Movie
+    public class Movie : Product
     {
         #region Private fields
-        private string title;
-        private decimal price;
         private string director;
-        private string imageUrl;
         private string producer;
         #endregion
 
         #region Public properties
-        public string Title
-        {
-            get { return title; } // read
-        }
-
-        public decimal Price
-        {
-            set
-            {
-                if (value <= 0)
-                {
-                    throw new Exception("Price is not accepted");
-                }
-                else
-                {
-                    price = value;
-                }
-            }
-            get { return price; }
-        }
-
-
-        public string ImageUrl
-        {
-            get
-            {
-                return imageUrl ?? string.Empty;
-            }
-            set
-            {
-                if (string.IsNullOrEmpty(value) == false)
-                {
-                    imageUrl = value;
-                }
-            }
-        }
 
         public string Director
         {
@@ -72,17 +31,13 @@ namespace MbmStore.Models
         #endregion
 
         // constructors
-        public Movie(string title, decimal price)
+        public Movie(string title, decimal price) : base(title, price)
         {
-            this.title = title;
-            this.price = price;
         }
 
-        public Movie(string title, decimal price, string imageUrl, string director)
+        public Movie(string title, decimal price, string imageUrl, string director) : base(title, price)
         {
-            this.title = title;
-            this.price = price;
-            ImageUrl = imageUrl;
+            this.ImageUrl = imageUrl;
             this.director = director;
         }
     }
